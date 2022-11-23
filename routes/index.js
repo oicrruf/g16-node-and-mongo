04f486f5e710c6d2f29e1675417753cc0a4d2e78
oicrruf <victor.reyes@focusservices.com>
2022-11-22 21:11:16 -0600

    feat: sync .env file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const package = require("../package.json");
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/health", function (req, res, next) {
+  const { name, version } = package;
+
+  res.send({
+    name,
+    version,
+  });
 });
 
 module.exports = router;
