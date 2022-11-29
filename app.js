76a6328cf8519503d50ca3b3a261410f8dda7969
Alelsito <lbalelxander@gmail.com>
2022-11-28 19:25:15 -0600

    feat: update validations

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const mongoose = require("mongoose");
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const { default: mongoose } = require('mongoose')
 
-const indexRouter = require("./routes/index");
+const indexRouter = require('./routes/index')
 
-require("dotenv").config();
-const app = express();
+require('dotenv').config()
+const app = express()
 
-// view engine setup
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
 mongoose
   .connect(process.env.URI)
-  .then(() => console.log("Your database is connected 😎"))
-  .catch((err) => console.log(err));
+  .then(() => console.log('Database connected 🤙'))
+  .catch((err) => console.log(err))
 
-app.use("/", indexRouter);
+app.use('/', indexRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-module.exports = app;
+module.exports = app
