6ea1e5cb41b2f1dc286f5bcb2b27430602a6f7f4
Alelsito <lbalelxander@gmail.com>
2022-11-30 20:36:48 -0600

    feat: find brand by name and id

--- routes/brand.js
+++ routes/brand.js
@@ -3,8 +3,24 @@ const router = express.Router()
 const { validationsBrand } = require('../middleware/brand')
 const { Brand } = require("../model");
 
-router.get("/", function (req, res, next) {
-  res.status(200).send(["Brand"]);
+router.get("/find", function (req, res, next) {
+  Brand.find({ name: req.query.name }, function (err, docs) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.status(200).send({ data: docs });
+    }
+  });
+});
+
+router.get("/:id", function (req, res, next) {
+  Brand.findById({ _id: req.params.id }, function (err, docs) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.status(200).send({ data: docs });
+    }
+  });
 });
 
 // brand/create
