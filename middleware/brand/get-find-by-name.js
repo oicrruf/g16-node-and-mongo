5c6b46bd2ea71228358baaef6c261369bdc63758
AleRMMM <karen.ramirez.18@hotmail.com>
2022-12-01 20:46:35 -0600

    feat:brand

---
+++ middleware/brand/get-find-by-name.js
@@ -0,0 +1,38 @@
+const yup = require("yup");
+
+const validationsFindByNameBrand = (req, res, next) => {
+  
+  let isValid = false;
+  let message = "";
+
+  const { name } = req.query;
+
+  const schema = yup.object().shape({
+    name: yup.string().required().strict(),
+  });
+
+  schema
+    .validate({
+      name
+    })
+    .then(function (valid) {
+      isValid = valid;
+    })
+    .catch((err) => {
+      message = err;
+    })
+    .then(() => {
+      if (isValid) {
+       next();
+      } else {
+        res.send({
+          error: {
+            type: message.name,
+            message: message.errors[0],
+          },
+        });
+      }
+    });
+};
+
+module.exports = { validationsFindByNameBrand };
