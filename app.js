6ae363a01cb62e5c01800f0269a8752cea3eb611
Alelsito <lbalelxander@gmail.com>
2022-12-06 20:54:28 -0600

    feat: disable clg warnings and delete non used next parameter for app file

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -44,7 +45,7 @@ app.use((req, res, next) => {
 });
 
 // error handler
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
