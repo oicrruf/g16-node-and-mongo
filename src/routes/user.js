b09a5464bea326fe7db98096e56911c464260808
Alelsito <lbalelxander@gmail.com>
2022-12-14 21:14:31 -0600

    feat: secure user routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,89 +1,107 @@
 const express = require('express');
 
 const router = express.Router();
-const { validationsUser } = require('../middleware/user');
+// const { validationsUser } = require('../middleware/user');
+const passport = require('passport');
 const { User } = require('../model');
 
+/* En el archivo auth.js ya existe una ruta para hacer signup (crear nuevo usuario) */
 // Create
-router.post('/', validationsUser, (req, res) => {
-  const user = new User();
-  user.first_name = req.body.first_name;
-  user.last_name = req.body.last_name;
-  user.email = req.body.email;
-  user.password = req.body.password;
-  user.gender = req.body.gender;
+// router.post('/', validationsUser, (req, res) => {
+//   const user = new User();
+//   user.first_name = req.body.first_name;
+//   user.last_name = req.body.last_name;
+//   user.email = req.body.email;
+//   user.password = req.body.password;
+//   user.gender = req.body.gender;
 
-  user.save((error, userStored) => {
-    if (error) {
-      res.status(500).send({ message: error });
-    }
+//   user.save((error, userStored) => {
+//     if (error) {
+//       res.status(500).send({ message: error });
+//     }
 
-    res.status(201).send({ user: userStored });
-  });
-});
+//     res.status(201).send({ user: userStored });
+//   });
+// });
+
+// Find by email
+router.get(
+  '/find',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    User.find({ email: req.query.email }, (err, docs) => {
+      if (err) {
+        throw err;
+      } else {
+        res.status(200).send({ data: docs });
+      }
+    });
+  }
+);
 
 // Find by id
-router.get('/:id', (req, res) => {
-  User.findById({ _id: req.params.id }, (err, docs) => {
-    if (err) {
-      throw err;
-    } else {
-      res.status(200).send({ data: docs });
-    }
-  });
-});
-
-// Find by name
-router.get('/find', (req, res) => {
-  User.find({ name: req.query.name }, (err, docs) => {
-    if (err) {
-      throw err;
-    } else {
-      res.status(200).send({ data: docs });
-    }
-  });
-});
+router.get(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    User.findById({ _id: req.params.id }, (err, docs) => {
+      if (err) {
+        throw err;
+      } else {
+        res.status(200).send({ data: docs });
+      }
+    });
+  }
+);
 
 // Find all
-router.get('/find/all', (res) => {
-  User.find({}, (err, docs) => {
-    if (err) {
-      throw err;
-    } else {
-      res.status(200).send({ data: docs });
-    }
-  });
-});
+router.get(
+  '/find/all',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    User.find({}, (err, docs) => {
+      if (err) {
+        throw err;
+      } else {
+        res.status(200).send({ data: docs });
+      }
+    });
+  }
+);
 
 // Update
-router.patch('/update', (req, res) => {
-  const key = Object.keys(req.query)[0];
-  User.findOneAndUpdate(
-    { [key]: req.query[key] }, // Valor buscado
-    { [key]: req.body.value }, // Nuevo valor
-    (err, docs) => {
-      if (err) {
-        throw err;
-      } else {
-        res.status(200).send({ data: docs });
+router.patch(
+  '/update',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const key = Object.keys(req.query)[0];
+    User.findOneAndUpdate(
+      { [key]: req.query[key] }, // Valor buscado
+      { [key]: req.body.value }, // Nuevo valor
+      (err, docs) => {
+        if (err) {
+          throw err;
+        } else {
+          res.status(200).send({ data: docs });
+        }
       }
-    }
-  );
-});
+    );
+  }
+);
 
 // Delete by id
-router.delete('/:id', (req, res) => {
-  User.deleteOne(
-    { _id: req.params.id },
-
-    (err, docs) => {
+router.delete(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    User.deleteOne({ _id: req.params.id }, (err, docs) => {
       if (err) {
         throw err;
       } else {
         res.status(200).send({ data: docs });
       }
-    }
-  );
-});
+    });
+  }
+);
 
 module.exports = router;
