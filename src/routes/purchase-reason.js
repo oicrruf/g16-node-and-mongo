8ea1235f12c27573f69649a85f5d23ac23bbc798
Alelsito <lbalelxander@gmail.com>
2022-12-14 21:21:03 -0600

    feat: secure purchase-reason routes

--- src/routes/purchase-reason.js
+++ src/routes/purchase-reason.js
@@ -2,88 +2,114 @@
 const express = require('express');
 
 const router = express.Router();
+const passport = require('passport');
 const {
   validationsCreatePurchaseReason,
   validationsFindByNamePurchaseReason,
 } = require('../middleware/purchase-reason');
-
 const { PurchaseReason } = require('../model');
 
 // Create
-router.post('/', validationsCreatePurchaseReason, (req, res) => {
-  const purchaseReason = new PurchaseReason();
-  purchaseReason.name = req.body.name;
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  validationsCreatePurchaseReason,
+  (req, res) => {
+    const purchaseReason = new PurchaseReason();
+    purchaseReason.name = req.body.name;
 
-  purchaseReason.save((error, purchaseReasonStored) => {
-    if (error) {
-      res.status(500).send({ message: error });
-    }
-    res.status(201).send(purchaseReasonStored);
-  });
-});
-
-// Find by id
-router.get('/:id', (req, res) => {
-  PurchaseReason.findById({ _id: req.params.id }, (err, docs) => {
-    if (err) {
-      res.status(404).send({ name: err.name, message: err.message });
-    } else {
-      res.status(200).send({ data: docs });
-    }
-  });
-});
+    purchaseReason.save((error, purchaseReasonStored) => {
+      if (error) {
+        res.status(500).send({ message: error });
+      }
+      res.status(201).send(purchaseReasonStored);
+    });
+  }
+);
 
 // Find by name
-router.get('/find', validationsFindByNamePurchaseReason, (req, res) => {
-  PurchaseReason.find({ name: req.query.name }, (err, docs) => {
-    if (err) {
-      throw err;
-    } else {
-      res.status(200).send({ data: docs });
-    }
-  });
-});
+router.get(
+  '/find',
+  passport.authenticate('jwt', { session: false }),
+  validationsFindByNamePurchaseReason,
+  (req, res) => {
+    PurchaseReason.find({ name: req.query.name }, (err, docs) => {
+      if (err) {
+        throw err;
+      } else {
+        res.status(200).send({ data: docs });
+      }
+    });
+  }
+);
+
+// Find by id
+router.get(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    PurchaseReason.findById({ _id: req.params.id }, (err, docs) => {
+      if (err) {
+        res.status(404).send({ name: err.name, message: err.message });
+      } else {
+        res.status(200).send({ data: docs });
+      }
+    });
+  }
+);
 
 // Find all
-router.get('/find/all', (req, res) => {
-  PurchaseReason.find({}, (err, docs) => {
-    if (err) {
-      throw err;
-    } else {
-      res.status(200).send({ data: docs });
-    }
-  });
-});
+router.get(
+  '/find/all',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    PurchaseReason.find({}, (err, docs) => {
+      if (err) {
+        throw err;
+      } else {
+        res.status(200).send({ data: docs });
+      }
+    });
+  }
+);
 
 // Update
-router.patch('/update', (req, res) => {
-  const key = Object.keys(req.query)[0];
-  PurchaseReason.findOneAndUpdate(
-    { [key]: req.query[key] }, // Valor buscado
-    { [key]: req.body.value }, // Nuevo valor
-    (err, docs) => {
-      if (err) {
-        throw err;
-      } else {
-        res.status(200).send({ data: docs });
+router.patch(
+  '/update',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const key = Object.keys(req.query)[0];
+    PurchaseReason.findOneAndUpdate(
+      { [key]: req.query[key] }, // Valor buscado
+      { [key]: req.body.value }, // Nuevo valor
+      (err, docs) => {
+        if (err) {
+          throw err;
+        } else {
+          res.status(200).send({ data: docs });
+        }
       }
-    }
-  );
-});
+    );
+  }
+);
 
 // Delete by id
-router.delete('/:id', (req, res) => {
-  PurchaseReason.deleteOne(
-    { _id: req.params.id },
+router.delete(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    PurchaseReason.deleteOne(
+      { _id: req.params.id },
 
-    (err, docs) => {
-      if (err) {
-        throw err;
-      } else {
-        res.status(200).send({ data: docs });
+      (err, docs) => {
+        if (err) {
+          throw err;
+        } else {
+          res.status(200).send({ data: docs });
+        }
       }
-    }
-  );
-});
+    );
+  }
+);
 
 module.exports = router;
